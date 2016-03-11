fbb5484fc4e2d05e45396eb30e8fc0678cddb9eb
Thomas VERMEILH <tvermeil@student.42.fr>
2016-03-11 16:52:45 +0100

    printf added

--- include/libft/printf/libft.h
+++ include/libft/printf/libft.h
@@ -6,7 +6,7 @@
 /*   By: tvermeil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 12:24:10 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/03/11 16:49:44 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/01/05 15:53:05 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strstr(const char *s1, const char *s2);
 char				*ft_strnstr(const char *s1, const char *s2, size_t n);
+char				*ft_strrev(char *str);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
 char				**ft_strsplit(char	const *s, char c);
-char				*ft_itoa_base(unsigned long long n, char *charset);
 char				*ft_itoa(int n);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
@@ -53,12 +53,13 @@ int					ft_isascii(int c);
 int					ft_isprint(int c);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
+char				*ft_tostr(char c);
 void				ft_putchar(char c);
 void				ft_putstr(char const *str);
+void				ft_putstr_spec(char *str);
 void				ft_putendl(char const *s);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
-void				ft_putstr_spec(char *s);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr(int n);
 void				ft_putnbr_fd(int n, int fd);
@@ -82,8 +83,5 @@ void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-char				*ft_tostr(char c);
-int					ft_write_big_endian(long long n, void *dst, size_t length);
-long long			ft_read_big_endian(void *src, size_t length);
 
 #endif
