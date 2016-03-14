828f3f666192379ce744440f97d06339cbb4f475
Orycterope <tvermeilh@hotmail.fr>
2016-03-14 16:16:38 +0100

    added winner display

--- src/vm/main.c
+++ src/vm/main.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "arena.h"
 #include "parameters.h"
 #include "execution.h"
@@ -54,6 +55,22 @@ static void	check_param(int *argc, char ***argv, t_arena *arena)
 	*argc -= 1;
 }
 
+static void	print_winner(t_arena *arena)
+{
+	t_player	*p;
+	t_player	*best;
+
+	best = p = arena->players;
+	while (p != NULL)
+	{
+		if (p->last_live >= best->last_live)
+			best = p;
+		p = p->next;
+	}
+	ft_printf("The fight has ended with the last survivor dying at cycle %d.\n\
+The winner is %s ! Its last words : \"%s\".",
+best->last_live, best->name, best->comment);
+}
 int				main(int argc, char **argv)
 {
 	t_arena	*arena;
@@ -66,6 +83,7 @@ int				main(int argc, char **argv)
 	display_champions(arena);
 	start_fight(arena);
 	dump_memory(arena);
+	print_winner(arena);
 	destroy_arena(arena);
 	return (0);
 }
