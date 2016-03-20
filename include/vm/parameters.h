3c8bd33a92ef0c293dceb881235e9e18879a9c65
Thomas VERMEILH <tvermeil@student.42.fr>
2016-03-20 17:40:13 +0100

    changed param.h

--- include/vm/parameters.h
+++ include/vm/parameters.h
@@ -6,7 +6,7 @@
 /*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/13 20:35:06 by adubedat          #+#    #+#             */
-/*   Updated: 2016/03/20 17:21:39 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/03/20 17:39:51 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void			decode_ocp(t_process *process, t_parameters *param, int i);
 void			params_value(t_process *process, t_parameters *param, int j);
 int				check_registers(t_parameters *param, t_process *process, int j);
 int				execute_instruction(t_process *process);
-int				check_param_error(t_process *process, t_parameters params, int i);
+int				check_param_error(t_parameters params, int i);
 void			change_carry(t_process *process, unsigned int value);
 
 #endif
