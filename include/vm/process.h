ba0e5470e61fa696ca7d589da3d0fae1dfc773f5
Thomas VERMEILH <tvermeil@student.42.fr>
2016-03-23 15:24:23 +0100

    registers are not malloc'd anymore

--- include/vm/process.h
+++ include/vm/process.h
@@ -6,20 +6,22 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/08 18:42:10 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/03/14 19:18:43 by adubedat         ###   ########.fr       */
+/*   Updated: 2016/03/23 14:51:08 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROCESS_H
 # define PROCESS_H
 # include "arena.h"
+# include "op.h"
+# define TP t_process
 
 typedef struct			s_arena t_arena;
 
 typedef struct			s_process
 {
 	int					player; //
-	char				**registers;
+	char				registers[REG_NUMBER][REG_SIZE];
 	char				*pc;
 	char				carry;
 	int					cycles_to_wait;
@@ -29,7 +31,7 @@ typedef struct			s_process
 }						t_process;
 
 void					fork_process(t_process *process, char *pc);
-void					create_process(int p, char *pc, t_arena *a, char **reg);
+void					create_process(int p, char *pc, t_arena *a, TP *parent);
 void					kill_process(t_process *process);
 void					execute_process(t_process *process);
 
--- src/vm/process.c
+++ src/vm/process.c
@@ -6,7 +6,7 @@
 /*   By: tvermeil <tvermeil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/08 19:55:35 by tvermeil          #+#    #+#             */
-/*   Updated: 2016/03/20 16:34:55 by tvermeil         ###   ########.fr       */
+/*   Updated: 2016/03/23 14:56:17 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,23 @@
 #include "execution.h"
 #include "libft.h"
 
-static char **create_copy_registers(char **registers, int player_id)
+static void create_copy_registers(t_process *p, t_process *parent, int player)
 {
-	int			i;
-	char		**new;
+	int		i;
 
-	new = (char **)malloc(sizeof(char *) * REG_NUMBER);
-	if (new == NULL)
-		return (NULL);
 	i = -1;
 	while (++i < REG_NUMBER)
 	{
-		new[i] = (char *)malloc(REG_SIZE);
-		if (new[i] == NULL)
-			new[i] = NULL;
-		else if (registers == NULL)
+		if (parent == NULL)
 		{
 			if (i == 0)
-				ft_write_big_endian(player_id, new[0], REG_SIZE);
+				ft_write_big_endian(player, p->registers[0], REG_SIZE);
 			else
-				ft_bzero(new[i], REG_SIZE);
+				ft_bzero(p->registers[i], REG_SIZE);
 		}
 		else
-			ft_memcpy(new[i], registers[i], REG_SIZE);
+			ft_memcpy(p->registers[i], parent->registers[i], REG_SIZE);
 	}
-	return (new);
 }
 
 /** Allocates a new process structure
@@ -46,33 +38,32 @@ static char **create_copy_registers(char **registers, int player_id)
  ** otherwise they are copied from existent registers
  **/
 
-void	create_process(int player, char *pc, t_arena *arena, char **registers)
+void	create_process(int player, char *pc, t_arena *arena, t_process *parent)
 {
 	t_process	*new;
 
-	new = (t_process*)ft_memalloc(sizeof(t_process));
+	new = (t_process*)malloc(sizeof(t_process));
+	if (new == NULL)
+		return ;
 	new->player = player;
-	new->registers = create_copy_registers(registers, player);
 	new->pc = pc;
-	new->carry = 0;
+	new->carry = (parent == NULL) ? 0 : parent->carry;
+	new->lives = (parent == NULL) ? 0 : parent->lives;
 	new->cycles_to_wait = get_wait_time(new) - 1;
-	new->lives = 0;
 	new->arena = arena;
 	new->next = arena->processes;
+	create_copy_registers(new, parent, player);
 	arena->processes = new;
 }
 
 void	fork_process(t_process *process, char *pc)
 {
-	create_process(process->player, pc, process->arena, process->registers);
-	process->arena->processes->carry = process->carry;
-	process->arena->processes->lives = process->lives;
+	create_process(process->player, pc, process->arena, process);
 }
 
 void	kill_process(t_process *process)
 {
 	t_process	*i;
-	int			r;
 
 	if (process->arena->processes == process)
 		process->arena->processes = process->next;
@@ -82,13 +73,6 @@ void	kill_process(t_process *process)
 		while (i->next != process)
 			i = i->next;
 		i->next = process->next;
-	}
-	if (process->registers != NULL)
-	{
-		r = -1;
-		while (++r < REG_NUMBER && process->registers[r] != NULL)
-			free(process->registers[r]);
-		free(process->registers);
 	}
 	free(process);
 }
