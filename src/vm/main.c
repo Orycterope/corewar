9269f2caf8d272a657b495c31877ddb25b96cc42
Arthur DUBEDAT <adubedat@e3r8p4.42.fr>
2016-05-07 18:56:18 +0200

    debug affichage winner

--- src/vm/main.c
+++ src/vm/main.c
@@ -6,7 +6,7 @@
 /*   By: adubedat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/11 19:08:45 by adubedat          #+#    #+#             */
-/*   Updated: 2016/05/07 18:44:43 by adubedat         ###   ########.fr       */
+/*   Updated: 2016/05/07 18:55:41 by adubedat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ static void	print_winner(t_arena *arena)
 //	printf("The fight has ended with the last survivor dying at cycle %d.\n\
 //The winner is %s ! Its last words : \"%s\".\n",
 //best->last_live, best->name, best->comment);
-	printf("Contestant %d, \"%s\" has won !", best->id, best->name);
+	printf("Contestant %d, \"%s\", has won !", best->id, best->name);
 }
 
 int				main(int argc, char **argv)
